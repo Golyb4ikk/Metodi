ec39edb7e778aa0e0a895ad75655fc29a3ab139f
Golyb4ik <only_game2@mail.ru>
2023-01-08 22:44:30 +0300

    Zadanie

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         if (year % 4 > 0 || year % 100 == 0) {
             System.out.println(year + " год не является високосным");
         } else {
-            System.out.println(year + " год является високосным");
+            System.out.println(year + " год является високосным!");
         }
         return year;
     }
